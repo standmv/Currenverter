1d97e01d39406aebf81c3ea1eca59f791fcb8c8d
Stanley De Moya <standmv13@gmail.com>
2018-03-11 18:26:46 -0400

    commit #1

--- app/src/main/java/com/demoyageek/currenverter/MainActivity.java
+++ app/src/main/java/com/demoyageek/currenverter/MainActivity.java
@@ -2,12 +2,30 @@ package com.demoyageek.currenverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private String[] data = {"Dog", "Cat", "Mouse"};
+    private Spinner mSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSpinner = findViewById(R.id.spinner_one);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSpinner.setAdapter(adapter);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this, mSpinner.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
     }
 }
